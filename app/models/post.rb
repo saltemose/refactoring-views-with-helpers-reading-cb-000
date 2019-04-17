421c97f4ceb6e7d3f2d21cbbf490e8ae9e6e92c4
Sean Altemose <sean@seanaltemose.com>
2019-04-17 20:31:51 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case
   belongs_to :author
 
+  def last_updated
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
+
   private
 
   def is_title_case
